54e993f4a1569350acdeb5001580c8e95471e773
Pham Tuan Hung <L20220047@SSPTAJI.CZ>
2024-10-07 13:09:48 +0200

    zprava

--- app.js
+++ app.js
@@ -9,3 +9,29 @@ if ('serviceWorker' in navigator) {
             });
     })
 }
+
+function calculate(){
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const c = document.getElementById("c").value;
+
+    let res1;
+    let res2;
+
+    let disc = (b*b) - (4*a*c)
+    if(disc>=0){
+        if(disc>=0){
+            res1= (-b) /(2*a)
+            res2=(-b )/(2*a)
+        }
+        res1=(-b +Math.sqrt(disc))/(2*a);
+        res2=(-b -Math.sqrt(disc))/(2*a);
+
+    }
+    
+
+    else{
+        res="nelze najít výsledek v poli reálných čísel"
+    }
+
+}
